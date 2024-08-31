ca55d1ed0e71e81337d00dcaa6629105084692a1
mayank2130 <mayankthakur1712@gmail.com>
2024-08-31 17:11:33 +0530

    card, theme

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { ThreeDCardDemo } from "../Card";
+
+export const Projects = () => {
+  return (
+    <div className="">
+      <div className=" gap-6 flex-row flex">
+        <ThreeDCardDemo />
+        <ThreeDCardDemo />
+        <ThreeDCardDemo />
+      </div>
+    </div>
+  );
+};
