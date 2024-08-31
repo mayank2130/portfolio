7a888f6c87bd867104d6bdb1c8dd165394d75c1e
mayank2130 <mayankthakur1712@gmail.com>
2024-08-31 19:12:51 +0530

    hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Hero } from "@/components/Hero";
 import { Projects } from "@/components/Projects";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Projects />
-    </div>
+    <>
+      <Hero />
+      <div className="flex items-center justify-center h-screen">
+        <Projects />
+      </div>
+    </>
   );
 }
