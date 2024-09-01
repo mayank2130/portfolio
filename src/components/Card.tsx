0ac80fdc94abaff0b8b808320862f2d6bf81ee71
mayank2130 <mayankthakur1712@gmail.com>
2024-09-01 07:44:53 +0530

    projects card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -5,7 +5,13 @@ import React from "react";
 import { CardBody, CardContainer, CardItem } from "./ui/3d-card";
 import Link from "next/link";
 
-export function ThreeDCardDemo() {
+type projectProps = {
+  title: String;
+  description: String;
+  sourceLink: String;
+  siteLink:String;
+};
+export function ThreeDCardDemo({ title, description, sourceLink, siteLink }: projectProps) {
   return (
     <CardContainer className="inter-var">
       <CardBody className="bg-gray-50 relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-[320px] sm:w-[320px] md:w-[400px] lg:w-[26rem] xl:w-[28rem] h-auto rounded-xl p-4 sm:p-5 lg:p-6 border">
@@ -13,14 +19,14 @@ export function ThreeDCardDemo() {
           translateZ="50"
           className="text-base sm:text-lg lg:text-xl font-bold text-neutral-600 dark:text-white tracking-normal"
         >
-          Make things float in air
+          {title}
         </CardItem>
         <CardItem
           as="p"
           translateZ="60"
           className="text-xs sm:text-sm max-w-sm mt-2 dark:text-neutral-300 tracking-normal"
         >
-          Hover over this card to unleash the power of CSS
+          {description}
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-3 sm:mt-4">
           <Image
@@ -35,18 +41,20 @@ export function ThreeDCardDemo() {
           <CardItem
             translateZ={20}
             as={Link}
-            href="https://twitter.com/mannupaaji"
+            href={sourceLink}
             target="__blank"
             className="px-3 py-1.5 sm:px-4 sm:py-2 rounded-xl text-xs font-normal dark:text-white tracking-normal"
           >
-            Try now →
+            Source Code →
           </CardItem>
           <CardItem
             translateZ={20}
-            as="button"
+            as={Link}
+            href={siteLink}
+            target="__blank"
             className="px-3 py-1.5 sm:px-4 sm:py-2 rounded-xl tracking-normal bg-black dark:bg-white dark:text-black text-white text-xs font-bold"
           >
-            Sign up
+            Visit Site
           </CardItem>
         </div>
       </CardBody>
--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -2,11 +2,39 @@ import React from "react";
 import { ThreeDCardDemo } from "../Card";
 
 export const Projects = () => {
+  const details = [
+    {
+      title: "AI SASS Chatbot",
+      description:
+        "Add your own custom trained chatbot on your website, that answers your business specific questions.",
+      sourceLink: "https://github.com/mayank2130/sass-AI-chatbot",
+      siteLink: "",
+    },
+    {
+      title: "Freelance Website",
+      description:
+        "A interactive way to apply for gigs, a bidding system. Select people with lowest bids and of highest quality",
+      sourceLink: "https://github.com/mayank2130/bid-for-work",
+      siteLink: "https://bid-for-work-docs.vercel.app/web",
+    },
+    {
+      title: "Custom Phone Case",
+      description:
+        "Create your own custom phone case. Add images and choose texture options to make your drean phone case.",
+      sourceLink: "https://github.com/mayank2130/cobracase-dev",
+      siteLink: "https://cobracase-dev.vercel.app",
+    },
+  ];
   return (
     <div className="flex flex-row justify-center gap-5">
-      <ThreeDCardDemo />
-      <ThreeDCardDemo />
-      <ThreeDCardDemo />
+      {details.map((data) => (
+        <ThreeDCardDemo
+          title={data.title}
+          description={data.description}
+          sourceLink={data.sourceLink}
+          siteLink={data.siteLink}
+        />
+      ))}
     </div>
   );
 };
